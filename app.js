044c9d944f22b16e36fbba2b5d5f42b46c66bf51
Kuriakin Zeng <kuriakin@college.harvard.edu>
2017-04-28 13:24:27 +0800

    change how dotenv loads .env

--- app.js
+++ app.js
@@ -25,8 +25,8 @@ const upload = multer({ dest: path.join(__dirname, 'uploads') });
 /**
  * Load environment variables from .env file, where API keys and passwords are configured.
  */
-// dotenv.load({ path: '.env' });
-dotenv.config();
+dotenv.load({ path: '.env' });
+// dotenv.config();
 console.log("mongolab:",process.env.MONGOLAB_URI);
 
 /**
